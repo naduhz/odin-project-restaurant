063ae24e4f14191253fc81d4cf5cadb3e75bb6da
jordan <jleejx2000@gmail.com>
2021-08-04 19:51:55 +0800

    complete logo

--- src/components/header.js
+++ src/components/header.js
@@ -2,5 +2,9 @@ export function header() {
   const headerElement = document.createElement("header");
   headerElement.className = "header";
 
+  const headerLogo = document.createElement("a");
+  headerLogo.className = "header-logo";
+  headerElement.appendChild(headerLogo);
+
   return headerElement;
 }
