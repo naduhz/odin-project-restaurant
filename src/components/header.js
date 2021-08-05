dd2774b259efbb8f4fafc1b773733d5f8ed347c3
jordan <jleejx2000@gmail.com>
2021-08-05 15:40:10 +0800

    header styled

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,21 @@
 export function header() {
   const headerElement = document.createElement("header");
-  headerElement.className = "header";
 
   const headerLogo = document.createElement("a");
   headerLogo.className = "header-logo";
   headerElement.appendChild(headerLogo);
+
+  const menuButton = document.createElement("a");
+  menuButton.className = "genericButton";
+  menuButton.id = "menuButton";
+  menuButton.textContent = "Menu";
+  headerElement.appendChild(menuButton);
+
+  const contactButton = document.createElement("a");
+  contactButton.className = "genericButton";
+  contactButton.id = "contactButton";
+  contactButton.textContent = "Contact Us";
+  headerElement.appendChild(contactButton);
 
   return headerElement;
 }
