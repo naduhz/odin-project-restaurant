390fe448952a823690c9bf057fe51037fe35e149
jordan <jleejx2000@gmail.com>
2021-08-06 23:43:44 +0800

    try to make a foodItem class

---
+++ src/menu/food.js
@@ -0,0 +1,8 @@
+const foodItem = (name, description, image, rating) => {
+  const name = this.name;
+  const description = this.description;
+  const image = this.image;
+  const rating = this.rating;
+
+  return { name, description, image, rating };
+};
