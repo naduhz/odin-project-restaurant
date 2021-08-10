70d502ee8e06b6b9cbdddd1731444550eeaf59f2
jordan <jleejx2000@gmail.com>
2021-08-10 21:58:41 +0800

    added more food

--- src/components/body.js
+++ src/components/body.js
@@ -1,6 +1,5 @@
-import pepperoniImg from "../assets/food/pepperoni-pizza.jpg";
+import { pepperoniPizza, hawaiianPizza, veganPizza } from "../menu/food";
 import middleImage from "../assets/Middle2.png";
-import foodItem from "../menu/food";
 
 export function home() {
   const bodyElement = document.createElement("section");
@@ -25,14 +24,7 @@ export function home() {
 }
 
 export function menu() {
-  const pepperoniPizza = foodItem(
-    "Pepperoni Pizza",
-    "Red base topped with spicy salami (contains beef and pork)",
-    pepperoniImg,
-    4.0
-  );
-
-  const allImages = [pepperoniPizza.img];
+  const allImages = [pepperoniPizza.img, hawaiianPizza.img, veganPizza.img];
 
   const bodyElement = document.createElement("section");
   bodyElement.className = "body menu";
@@ -40,7 +32,7 @@ export function menu() {
 
   const menuImages = document.createElement("div");
   menuImages.id = "menuImages";
-  bodyElement.appendChild(allImages[0]);
+  bodyElement.appendChild(allImages);
 
   const orderButton = document.createElement("a");
   orderButton.className = "genericButton";
--- src/menu/food.js
+++ src/menu/food.js
@@ -1,3 +1,7 @@
+import pepperoniImg from "../assets/food/pepperoni-pizza.jpg";
+import hawaiianImg from "../assets/food/hawaiian-pizza.jpg";
+import veganImg from "../assets/food/vegan-pizza.jpg";
+
 const foodItem = (name, description, img_src, rating) => {
   const img = new Image();
   img.src = img_src;
@@ -5,4 +9,25 @@ const foodItem = (name, description, img_src, rating) => {
   return { name, description, img, rating };
 };
 
-export default foodItem;
+const pepperoniPizza = foodItem(
+  "Pepperoni Pizza",
+  "Red base topped with spicy salami (contains beef and pork)",
+  pepperoniImg,
+  4.2
+);
+
+const hawaiianPizza = foodItem(
+  "Hawaiian Pizza",
+  "Red base topped with savoury chicken ham, melty mozzarella cheese and pineapples.",
+  hawaiianImg,
+  3.8
+);
+
+const veganPizza = foodItem(
+  "Vegan Pizza",
+  "Vegan cheese, spinach, mixed peppers, olives and mushrooms.",
+  veganImg,
+  3.5
+);
+
+export { pepperoniPizza, hawaiianPizza, veganPizza };
